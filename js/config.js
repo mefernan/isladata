195fdc1d893f5335c5370845e4010f2021287b3f
Mario Fernandez <mefernandez@unimagdalena.com>
2025-06-25 17:00:42 -0500

    Update url ghost-cms

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 const ghostConfig = {
-  url: 'http://localhost:2368',        // ← cámbialo por tu dominio Ghost
-  key: '34a6a4391d3b55046e8e1ee74e',                 // ← tu Content API Key aquí
-  limit: 5                                           // número de posts a mostrar
+  url: 'https://ghost-cms-isladata.onrender.com/',    // url ghost-cms
+  key: '34a6a4391d3b55046e8e1ee74e',                 // Content API Key
+  limit: 5                                           // posts a mostrar
 };
