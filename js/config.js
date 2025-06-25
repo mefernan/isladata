db3e995c236ce1f33b664b6a7a0799fbe4e229fd
Mario Fernandez <mefernandez@unimagdalena.com>
2025-06-25 17:03:27 -0500

    delete / in url ghost-cms

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // config.js
 
 const ghostConfig = {
-  url: 'https://ghost-cms-isladata.onrender.com/',    // url ghost-cms
+  url: 'https://ghost-cms-isladata.onrender.com',    // url ghost-cms
   key: '34a6a4391d3b55046e8e1ee74e',                 // Content API Key
   limit: 5                                           // posts a mostrar
 };
